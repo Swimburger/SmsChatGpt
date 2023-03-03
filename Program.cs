320068e773cecd70ad33e106e0d7fd792955c783
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-03-03 18:44:25 -0500

    Use new ChatGPT API

--- Program.cs
+++ Program.cs
@@ -1,46 +1,197 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using OpenAI.GPT3.Extensions;
 using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.ObjectModels;
 using OpenAI.GPT3.ObjectModels.RequestModels;
+using System.Text;
+using System.Text.Json;
+using Twilio.AspNet.Core;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenAIService();
-
-var app = builder.Build();
-
-app.MapPost("/message", async (
-    IOpenAIService openAiService,
-    HttpRequest request,
-    CancellationToken cancellationToken
-) =>
+public class Program
 {
-    var form = await request.ReadFormAsync(cancellationToken).ConfigureAwait(false);
-    var body = form["Body"].ToString();
-    var completionResult = await openAiService.Completions.CreateCompletion(
-        new CompletionCreateRequest()
-        {
-            Prompt = body,
-            Model = Models.TextDavinciV3,
-            //A number between 0 and 1 that determines how many creative risks the engine takes when generating text.
-            Temperature = 0.7f,
-            // Maximum completion length
-            MaxTokens = 3000,
-            // # between -2.0 and 2.0. The higher this value, the bigger the effort the model will make in not repeating itself.
-            FrequencyPenalty = 0.7f
-        }
-    ).ConfigureAwait(false);
-
-    if (!completionResult.Successful)
+    private const string PreviousMessagesKey = "PreviousMessages";
+    private static readonly CookieOptions CookieOptions = new()
     {
-        if (completionResult.Error == null) throw new Exception("An unexpected error occurred.");
-        var errorMessage = completionResult.Error.Code ?? "";
-        if (errorMessage != "") errorMessage += ": ";
-        errorMessage += completionResult.Error.Message;
-        throw new Exception(errorMessage);
+        HttpOnly = true,
+        SameSite = SameSiteMode.Strict,
+        Secure = true
+    };
+
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDistributedMemoryCache();
+
+        builder.Services.AddSession(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
+
+        builder.Services.Configure<ForwardedHeadersOptions>(options => options.ForwardedHeaders = ForwardedHeaders.All);
+
+        builder.Services
+            .AddTwilioClient()
+            .AddTwilioRequestValidation();
+
+        builder.Services.AddOpenAIService();
+
+        var app = builder.Build();
+
+        app.UseSession();
+
+        app.UseForwardedHeaders();
+
+        app.MapPost("/message", async (
+            HttpContext context,
+            CancellationToken cancellationToken,
+            IOpenAIService openAiService,
+            ITwilioRestClient twilioClient
+        ) =>
+        {
+            var request = context.Request;
+            var response = context.Response;
+            var session = context.Session;
+            await session.LoadAsync(cancellationToken).ConfigureAwait(false);
+
+            var form = await request.ReadFormAsync(cancellationToken).ConfigureAwait(false);
+            var receivedFrom = form["From"].ToString();
+            var sentTo = form["To"].ToString();
+            var body = form["Body"].ToString().Trim();
+
+            if (body.Equals("reset", StringComparison.OrdinalIgnoreCase))
+            {
+                RemovePreviousMessages(session);
+                var __ = MessageResource.CreateAsync(
+                    to: receivedFrom,
+                    from: sentTo,
+                    body: "Your conversation is now reset.",
+                    client: twilioClient
+                );
+                return Results.Ok();
+            }
+
+            var messages = GetPreviousMessages(session);
+
+            messages.Add(ChatMessage.FromUser(body));
+            string chatResponse = await GetChatResponse(openAiService, receivedFrom, messages);
+
+            messages.Add(ChatMessage.FromAssistance(chatResponse));
+            SetPreviousMessages(session, messages);
+
+            var responseMessages = SplitResponseInChunks(chatResponse);
+
+            var _ = SendResponse(twilioClient, to: receivedFrom, from: sentTo, responseMessages);
+
+            return Results.Ok();
+        });
+
+        app.Run();
     }
 
-    return Results.Text(completionResult.Choices[0].Text.Trim());
-});
+    private static List<string> SplitResponseInChunks(string chatResponse)
+    {
+        List<string> messages = new();
+        var paragraphs = chatResponse.Split("\n\n");
 
-app.Run();
+        StringBuilder messageBuilder = new();
+        for (int paragraphIndex = 0; paragraphIndex < paragraphs.Length - 1; paragraphIndex++)
+        {
+            string currentParagraph = paragraphs[paragraphIndex];
+            string nextParagraph = paragraphs[paragraphIndex + 1];
+            messageBuilder.Append(currentParagraph);
+
+            // 320 is the recommended message length for maximum deliverability
+            // + 2 for "\n\n"
+            if (messageBuilder.Length + nextParagraph.Length > 320 + 2)
+            {
+                messages.Add(messageBuilder.ToString());
+                messageBuilder.Clear();
+            }
+            else
+            {
+                messageBuilder.Append("\n\n");
+            }
+        }
+
+        messageBuilder.Append(paragraphs.Last());
+        messages.Add(messageBuilder.ToString());
+
+        return messages;
+    }
+
+    private static async Task<string> GetChatResponse(
+        IOpenAIService openAiService,
+        string from,
+        List<ChatMessage> messages
+    )
+    {
+        var completionResult = await openAiService.ChatCompletion.CreateCompletion(
+            new ChatCompletionCreateRequest
+            {
+                Messages = messages,
+                Model = Models.ChatGpt3_5Turbo,
+                User = from
+            }
+        );
+
+        if (!completionResult.Successful)
+        {
+            if (completionResult.Error == null) throw new Exception("An unexpected error occurred.");
+            var errorMessage = completionResult.Error.Code ?? "";
+            if (errorMessage != "") errorMessage += ": ";
+            errorMessage += completionResult.Error.Message;
+            throw new Exception(errorMessage);
+        }
+
+        var chatResponse = completionResult.Choices[0].Message.Content.Trim();
+        return chatResponse;
+    }
+
+
+    private static async Task SendResponse(
+        ITwilioRestClient twilioClient,
+        string to,
+        string from,
+        List<string> responseMessages
+    )
+    {
+        foreach (var responseMessage in responseMessages)
+        {
+            await MessageResource.CreateAsync(
+                to: to,
+                from: from,
+                body: responseMessage,
+                client: twilioClient
+            )
+            .ConfigureAwait(false);
+            await Task.Delay(1000);
+        }
+    }
+
+    private static List<ChatMessage> GetPreviousMessages(ISession session)
+    {
+        var json = session.GetString(PreviousMessagesKey);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return new List<ChatMessage>();
+        }
+
+        return JsonSerializer.Deserialize<List<ChatMessage>>(json)
+                ?? new List<ChatMessage>();
+    }
+
+    private static void SetPreviousMessages(ISession session, List<ChatMessage> messages)
+    {
+        var serializedJson = JsonSerializer.Serialize(messages);
+        session.SetString(PreviousMessagesKey, serializedJson);
+    }
+
+    private static void RemovePreviousMessages(ISession session)
+        => session.Remove(PreviousMessagesKey);
+}
